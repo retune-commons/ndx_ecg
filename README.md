e3ce029eb7d4b5739c303ac46146e2b43ca0a68e
Hamidreza-Alimohammadi <alimohammadi.hamidreza@gmail.com>
2023-09-24 20:20:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# ndx-ecg Extension for NWB
+
+Description of the extension
+
+## Installation
+
+
+## Usage
+
+```python
+
+```
+
+---
+This extension was created using [ndx-template](https://github.com/nwb-extensions/ndx-template).
---
+++ src/pynwb/ndx_ecg/__init__.py
@@ -0,0 +1,26 @@
+import os
+from pynwb import load_namespaces, get_class
+
+# Set path of the namespace.yaml file to the expected install location
+ndx_ecg_specpath = os.path.join(
+    os.path.dirname(__file__),
+    'spec',
+    'ndx-ecg.namespace.yaml'
+)
+
+# If the extension has not been installed yet but we are running directly from
+# the git repo
+if not os.path.exists(ndx_ecg_specpath):
+    ndx_ecg_specpath = os.path.abspath(os.path.join(
+        os.path.dirname(__file__),
+        '..', '..', '..',
+        'spec',
+        'ndx-ecg.namespace.yaml'
+    ))
+
+# Load the namespace
+load_namespaces(ndx_ecg_specpath)
+
+# TODO: import your classes here or define your class using get_class to make
+# them accessible at the package level
+TetrodeSeries = get_class('TetrodeSeries', 'ndx-ecg')
